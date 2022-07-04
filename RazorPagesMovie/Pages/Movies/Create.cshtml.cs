3d2eb99f6cbe6dbb99f26f8bacb6e7eab0ecf7d5
giodefa96 <giodefa96@gmail.com>
2022-07-04 12:36:53 +0200

    Message

--- RazorPagesMovie/Pages/Movies/Create.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -26,7 +26,6 @@ namespace RazorPagesMovie.Pages.Movies
 
         [BindProperty]
         public Movie Movie { get; set; } = default!;
-        
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
